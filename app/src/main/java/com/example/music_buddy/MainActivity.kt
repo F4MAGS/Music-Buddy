7ac96638a5ab9649a37a14b535c13cb66290ef25
pgoce <goce.partaloski@gmail.com>
2022-11-09 22:52:06 -0500

    First Commit

---
+++ app/src/main/java/com/example/music_buddy/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.music_buddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.music_buddy.ui.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
