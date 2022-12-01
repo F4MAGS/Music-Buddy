faf00e190c198074668faa1e2641f9f2d966d4af
Andrew Crews <ac235@njit.edu>
2022-12-01 13:18:35 -0500

    ChatXML Finished

---
+++ app/src/main/java/com/example/music_buddy/ui/fragments/chatFragment.kt
@@ -0,0 +1,11 @@
+package com.example.music_buddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chatFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_chat)
+    }
+}
